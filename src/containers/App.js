251f00d54327a3ba13244ded4593ad0d2a77095d
SirRobZ <sazarapkar@gmail.com>
2020-04-20 14:02:04 -0400

    added error boundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Cardlist from '../components/Cardlist';
 import Scroll from '../components/Scroll';
 import SearchBox from '../components/SearchBox';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
@@ -37,7 +38,9 @@ class App extends Component {
 				<h1 className='f1'>Robofriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<Scroll>
-					<Cardlist robots={filteredRobots}/>
+					<ErrorBoundary>
+						<Cardlist robots={filteredRobots}/>
+					</ErrorBoundary>
 				</Scroll>
 			</div>
 		);
